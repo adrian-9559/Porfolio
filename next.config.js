f75efde4aab76481a225e7981d604213a1ec9a90
Adrián Escribano Pérez <adrian.escribano3@gmail.com>
2024-11-27 23:22:02 +0100

    modified:   next.config.js 	modified:   package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/<adrian-9559.github.io>' : '',
+  assetPrefix: isProd ? '/<adrian-9559.github.io>/' : '',
+};
