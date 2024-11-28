0415bca0066b3550cead44f442e6e8f95b26d658
Adrián Escribano Pérez <adrian.escribano3@gmail.com>
2024-11-28 13:50:31 +0100

    modified:   next.config.js 	modified:   package.json

--- next.config.js
+++ next.config.js
@@ -6,12 +6,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-module.exports = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: isProd ? '/<adrian-9559.github.io>' : '',
-  assetPrefix: isProd ? '/<adrian-9559.github.io>/' : '',
-};
